dc4b106bd9c389225683a0d968eb641834e54dee
Chris <cvmiasto@gmail.com>
2021-03-22 09:33:13 -0400

    added api key to url

--- app/src/main/java/com/example/weatherapp/APICall.java
+++ app/src/main/java/com/example/weatherapp/APICall.java
@@ -6,7 +6,7 @@ import retrofit2.http.Query;
 
 public interface APICall {
 
-    @GET("current.json?key=  ENTER API KEY  &q=")
+    @GET("current.json?key=0707ff189ef94a7bb9e133029212203&q=")
     Call<WeatherModel> getWeather(@Query("q") String latLong);
 
 }
