17b20b5c0df61b9bdefe5c5f977b65167a1bd601
Chris <cvmiasto@gmail.com>
2021-03-23 23:55:16 -0400

    Update APICall.java
    Removed my API Key

--- app/src/main/java/com/example/weatherapp/APICall.java
+++ app/src/main/java/com/example/weatherapp/APICall.java
@@ -8,13 +8,13 @@ import retrofit2.http.Query;
 
 public interface APICall {
 
-    @GET("current.json?key=0707ff189ef94a7bb9e133029212203&q=")
+    @GET("current.json?key= Enter Your API Key here &q=")
     Call<WeatherModel> getWeather(@Query("q") String latLong);
 
 
 
 
-    @GET("forecast.json?key=0707ff189ef94a7bb9e133029212203&q=&days=1&aqi=no&alerts=no")
+    @GET("forecast.json?key=  Enter Your API Key Here  &q=&days=1&aqi=no&alerts=no")
     Call<WeatherModel> getHourDetails(@Query("q") String latLong);
 
 }
